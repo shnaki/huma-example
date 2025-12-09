8b59b923922edc7db27cfad4be91a07dab4924c6
shnaki <tehnca-nome@hotmail.co.jp>
2025-12-09 22:44:42 +0900

    refactor: テストしやすいようにリファクタリングを実施

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humago"
 	"github.com/danielgtaylor/huma/v2/humacli"
+
+	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
 )
 
-// Options for the CLI. Pass `--port` or set the `SERVICE_PORT` env var.
+// Options for the CLI.
 type Options struct {
 	Port int `help:"Port to listen on" short:"p" default:"8888"`
 }
@@ -31,6 +33,37 @@ type ReviewInput struct {
 	}
 }
 
+func addRoutes(api huma.API) {
+	// Register GET /greeting/{name}
+	huma.Register(api, huma.Operation{
+		OperationID: "get-greeting",
+		Method:      http.MethodGet,
+		Path:        "/greeting/{name}",
+		Summary:     "Get a greeting",
+		Description: "Get a greeting for a person by name.",
+		Tags:        []string{"Greetings"},
+	}, func(ctx context.Context, input *struct {
+		Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
+	}) (*GreetingOutput, error) {
+		resp := &GreetingOutput{}
+		resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
+		return resp, nil
+	})
+
+	// Register POST /reviews
+	huma.Register(api, huma.Operation{
+		OperationID:   "post-review",
+		Method:        http.MethodPost,
+		Path:          "/reviews",
+		Summary:       "Post a review",
+		Tags:          []string{"Reviews"},
+		DefaultStatus: http.StatusCreated,
+	}, func(ctx context.Context, i *ReviewInput) (*struct{}, error) {
+		// TODO: save review in data store.
+		return nil, nil
+	})
+}
+
 func main() {
 	// Create a CLI app which takes a port option.
 	cli := humacli.New(func(hooks humacli.Hooks, options *Options) {
@@ -38,34 +71,7 @@ func main() {
 		router := http.NewServeMux()
 		api := humago.New(router, huma.DefaultConfig("My API", "1.0.0"))
 
-		// Register GET /greeting/{name}
-		huma.Register(api, huma.Operation{
-			OperationID: "get-greeting",
-			Method:      http.MethodGet,
-			Path:        "/greeting/{name}",
-			Summary:     "Get a greeting",
-			Description: "Get a greeting for a person by name.",
-			Tags:        []string{"Greetings"},
-		}, func(ctx context.Context, input *struct {
-			Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
-		}) (*GreetingOutput, error) {
-			resp := &GreetingOutput{}
-			resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
-			return resp, nil
-		})
-
-		// Register POST /reviews
-		huma.Register(api, huma.Operation{
-			OperationID:   "post-review",
-			Method:        http.MethodPost,
-			Path:          "/reviews",
-			Summary:       "Post a review",
-			Tags:          []string{"Reviews"},
-			DefaultStatus: http.StatusCreated,
-		}, func(ctx context.Context, i *ReviewInput) (*struct{}, error) {
-			// TODO: save review in data store.
-			return nil, nil
-		})
+		addRoutes(api)
 
 		// Tell the CLI how to start your server.
 		hooks.OnStart(func() {
